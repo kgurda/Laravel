7adc693c88e795a7528852ca1dd8ac897f8b7092
Katarzyna Gurda <gurda.kasia@gmail.com>
2017-03-22 14:32:44 +0100

    create routes and controllers

---
+++ cms/app/Http/routes.php
@@ -0,0 +1,42 @@
+<?php
+
+
+Route::get('/', function () {
+
+    return view('welcome');
+});
+
+//Route::get('/about', function () {
+//
+//    return "Hi about page!";
+//
+//});
+//
+//Route::get('/contact', function () {
+//
+//    return "Hi contact page!";
+//
+//});
+//
+//Route::get('/post/{id}', function($id) {
+//
+//    return "this is post number " . $id;
+//
+//});
+//
+//Route::get('/admin/posts/exaple', array('as'=>'admin.home', function() {
+//
+//    $url = route('admin.home');
+//
+//    return "this url is " . $url;
+//
+//}));
+
+
+Route::get('/post/{id}', 'PostsController@index');
+
+
+Route::group(['middleware' => ['web']], function() {
+
+
+});
