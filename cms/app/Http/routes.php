8bf17004d61a664b0af2c006c276e984ff6d699f
Katarzyna Gurda <gurda.kasia@gmail.com>
2017-03-23 14:30:21 +0100

    create db  queries

--- cms/app/Http/routes.php
+++ cms/app/Http/routes.php
@@ -1,10 +1,11 @@
 <?php
 
 
-Route::get('/', function () {
-
-    return view('welcome');
-});
+//Route::get('/', function () {
+//
+//    return view('welcome');
+//
+//});
 
 //Route::get('/about', function () {
 //
@@ -36,6 +37,59 @@ Route::get('/', function () {
 //Route::get('/post/{id}', 'PostsController@index');
 
 //Route::resource('posts', 'PostsController');
+
+
+/*
+ * -----database sql querys--------
+ */
+
+Route::get('/insert', function () {
+
+    DB::insert('insert into posts(title, content) values(?,?)', ['PHP with Laravel', 'PHP Laravel is the best thing that has happened to PHP']);
+
+
+});
+
+Route::get('/read', function() {
+
+
+    $results = DB::select('select * from posts where id = ?', [1]);
+
+//    foreach ($results as $post) {
+//
+//        return $post->title;
+//    }
+
+    return $results; //give us object
+
+});
+
+Route::get('/update', function() {
+
+
+    $updated = DB::update('update posts set title = "Updated title" where id = ?', [1]);
+
+    return $updated;
+
+
+
+});
+
+Route::get('delete', function() {
+
+   $deleted = DB::delete('delete from posts where id = ?', [1]);
+
+   return $deleted;
+
+});
+
+
+/*
+ * -------------------------------------------
+ */
+
+
+
 
 Route::get('/contact', 'PostsController@contact');
 
