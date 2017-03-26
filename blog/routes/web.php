10b91d9f36c9b3ebd15a165f0963f76b9d66c984
Katarzyna Gurda <gurda.kasia@gmail.com>
2017-03-26 16:01:00 +0200

    new project blog, practicing laravel

---
+++ blog/routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/tasks', function () {
+
+    $tasks = \App\Task::all();
+
+    return view('tasks/index', compact('tasks'));
+});
+
+
+Route::get('/tasks/{id}', function ($id) {
+
+    $task = DB:: table('tasks')->find($id);
+
+    return view('tasks/show', compact('task'));
+});
