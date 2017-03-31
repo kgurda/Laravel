84c6bec295238630d1215b8afc9a01445cc616a8
Katarzyna Gurda <gurda.kasia@gmail.com>
2017-03-31 11:31:03 +0200

    AppServiceProvider modified

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -35,14 +35,9 @@ class PostsController extends Controller
 
 
 
-        $archives = Post::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-            ->groupBy('year', 'month')
-            ->orderByRaw('min(created_at) desc')
-            ->get()
-            ->toArray();
 
 
-        return view('posts.index', compact('posts', 'archives'));
+        return view('posts.index', compact('posts'));
     }
 
     public function show(Post $post)
